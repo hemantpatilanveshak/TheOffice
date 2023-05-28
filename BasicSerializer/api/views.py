65fe0a9278854f2d5adb62ef69ef921c741e2b0e
hemantpatilanveshak <hemant.patil@anveshak.com>
2023-05-28 19:13:54 +0530

    Get Objects

--- BasicSerializer/api/views.py
+++ BasicSerializer/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+### Function based View
+
+
+def student_details(request,id=None):
+
+    if id is not None:
+
+        stu = Student.objects.get(id = id)
+        serializer = StudentSerializer(stu)
+        json_data = JSONRenderer().render(serializer.data)
+
+        return HttpResponse(json_data, content_type='application/json')
+    
+    else:
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many = True)
+        json_data = JSONRenderer().render(serializer.data)
+
+        return HttpResponse(json_data, content_type='application/json')
+
+
+
