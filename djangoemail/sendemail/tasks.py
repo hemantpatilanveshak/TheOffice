c5f4f157fe320339edbadb3f7d1f718727cfe2c0
Hemant Patil <hemant.patil@anveshak.com>
2023-06-26 09:58:42 +0530

    Add

--- djangoemail/sendemail/tasks.py
+++ djangoemail/sendemail/tasks.py
@@ -1,6 +1,6 @@
 from . models import Student
 from celery import shared_task
-from django.core.mail import send_mail
+from django.core.mail import send_mail , EmailMessage
 from djangoemail import settings
 
 
@@ -13,12 +13,22 @@ def send_email_func(self):
         message = "Ignore the message Test from Celery "
         to_email =  person.email
 
-        send_mail(
-            subject=mail_subject,
-            message=message,
+        # send_mail(
+        #     subject=mail_subject,
+        #     message=message,
+        #     from_email=settings.EMAIL_HOST_USER,
+        #     recipient_list=[to_email],
+        #     fail_silently=True
+        # )
+        email = EmailMessage(
+            subject= mail_subject,
+            body= message,
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[to_email],
-            fail_silently=True
-        )
+            to= [to_email],
 
+        )
+        title = 'dummy'
+        email.attach_file('C:\TempWork\DRF\Books\media\pdf'+'\\'+ title +'.pdf')
+        email.send()
+        
     return "Done"
